37da18d50a8efa46eaeacde98553f8925af1f262
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-06-05 03:03:14 +0530

    create usermodel and setup tailwind css in fronend

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -2,11 +2,58 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
-
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
     fullName: {
-        type:String,
-        
-    }
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
   },
 
   { timestamps: true }
