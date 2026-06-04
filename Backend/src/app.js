064af7962c6d7f63bb304ee435c9a049441206a9
swadesh-231 <swadeshchatterjee512@gmail.com>
2026-06-04 11:35:51 +0530

    create a project management system backend

---
+++ Backend/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+// basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+
+export default app;
---
+++ Backend/src/constants.js
@@ -0,0 +1,17 @@
+export const DB_NAME = "projectmanagement";
+
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+
+export const AvailableUserRole = Object.values(UserRolesEnum);
+
+export const TaskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+};
+
+export const AvailableTaskStatues = Object.values(TaskStatusEnum);
